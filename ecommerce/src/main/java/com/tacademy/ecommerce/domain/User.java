4852ee4a5798e1f1812176f26dcbfbf6ee8897d1
jhan_kim <jhan_kim@459847a3-8e1c-0510-9d92-9ff6bfb05984>
2015-09-20 03:05:02 +0000

    security 설정 추가

--- ecommerce/src/main/java/com/tacademy/ecommerce/domain/User.java
+++ ecommerce/src/main/java/com/tacademy/ecommerce/domain/User.java
@@ -1,6 +1,5 @@
 package com.tacademy.ecommerce.domain;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -8,20 +7,21 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "t_user")
@@ -29,36 +29,43 @@ import org.springframework.security.core.userdetails.UserDetails;
 @EqualsAndHashCode(callSuper = false)
 public class User extends AbstractEntity<Long> {
 
-  private static final long serialVersionUID = -4427406588176074887L;
+	private static final long serialVersionUID = -4427406588176074887L;
 
-  @Id
-  @GeneratedValue
-  private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-  @ManyToOne
-  @JoinColumn(name = "role_id", referencedColumnName = "id")
-  protected Role role;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "t_user_role", joinColumns = {
+			@JoinColumn(name = "user_id") }, inverseJoinColumns = @JoinColumn(name = "role_id") )
+	private Set<Role> roles = new HashSet<Role>();
 
-  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private Cart cart;
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Cart cart;
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
-  @OrderBy("orderDate desc")
-  private Set<Order> orders = new HashSet<Order>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	@OrderBy("orderDate desc")
+	private Set<Order> orders = new HashSet<Order>();
 
-  @Column(length = 30)
-  private String name;
+	@Column(length = 30)
+	private String name;
 
-  @Column(length = 50)
-  private String username;
+	@Column(length = 50)
+	private String username;
 
-  @Column(length = 20)
-  private String password;
+	@Column(length = 20)
+	private String password;
 
-  @Column(length = 100)
-  private String email;
+	@Column(length = 100)
+	private String email;
 
-  @Column(length = 12)
-  private String mobile;
+	@Column(length = 12)
+	private String mobile;
+
+	public Set<GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities = new LinkedHashSet<GrantedAuthority>();
+		this.roles.forEach(role -> authorities.add(role));
+		return authorities;
+	}
 
 }
